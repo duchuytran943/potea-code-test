f096546d2c9860d28509c64d724678492acbf158
duchuytran943 <duchuytran943@gmail.com>
2021-03-12 01:22:29 +0700

    Fix bug error 429

---
+++ src/views/Error429.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <h1>429</h1>
+      </div>
+      <h2>{{ $t('error404.message') }}</h2>
+      <p>{{ $t('error404.description') }}</p>
+      <router-link :to="{ name: 'Home' }">{{ $t('general.backToHome') }}</router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Error429',
+};
+</script>
