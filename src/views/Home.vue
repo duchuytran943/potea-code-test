8cecfcbaa602802b08c6414029495055079acd3b
duchuytran943 <duchuytran943@gmail.com>
2021-03-11 21:52:31 +0700

    Setup API call images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,57 @@
 <template>
   <HomePageLayout>
-    <h1>This is home page</h1>
+    <p>
+      {{ images }}
+    </p>
   </HomePageLayout>
 </template>
 
 <script>
+import httpAxios from '@/httpAxios';
+
 export default {
   name: 'Home',
 
   components: {},
 
   data() {
-    return {};
+    return {
+      limit: 3,
+      offset: 1,
+      isLoading: false,
+      images: [],
+    };
   },
 
-  created() {},
+  created() {
+    this.getListImage();
+  },
 
-  methods: {},
+  methods: {
+    getListImage() {
+      const params = {
+        api_key: process.env.VUE_APP_PUBLIC_KEY_GIPHY,
+        limit: this.limit,
+        // offset: this.offset,
+      };
+      this.isLoading = true;
+      httpAxios({
+        url: `${this.$backendUrl}/v1/gifs/trending`,
+        method: 'GET',
+        params,
+      })
+        .then(response => {
+          console.log('data', response.data.data);
+          this.images = response.data.data;
+        })
+        .catch(e => {
+          console.log(e);
+          this.$router.push('Error404');
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+  },
 };
 </script>
